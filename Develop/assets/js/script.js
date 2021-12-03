32a2b6f63f0fea93f211aba14387a79cc4eebc57
Samuel Riveros <sam_riveros18@outlook.com>
2021-12-02 19:28:15 -0500

    I added a function that keeps up with the current time and adjust accordingly. It will gray out past time

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,14 +2,51 @@
 var today = moment();
 $('#currentDay').text(today.format('dddd, MMMM Do'));
 
-// creates a listerner for the save button
+// creates a listener for the save button
 $(document).ready(function(){
 
-    $(".saveBtn").on("click", function(){
-        var timeSaved = $(this).parent().attr("id");
-        var textSubmitted = $(this).siblings(".description").val();
+    $('.saveBtn').on('lick', function(){
+        var timeSaved = $(this).parent().attr('id');
+        var textSubmitted = $(this).siblings('.description').val();
         
         localStorage.setItem(timeSaved, textSubmitted); // saves user input to local storage in an array
-    })
+    })// closes listener 
 
-})
+    // a function that keeps up with current time and acts accordingly. Bloccks the ability to submit text on past time blocks
+    function liveTimeUpdate(){ 
+
+        var currentTime = moment().hour();
+
+        $('.time-block').each(function (){
+
+            var blockPastTime = parseInt(($this).attr('id').split('hour')[1]);// iterates through the schedule chart according to the time
+
+            // if the the current hour is greater than the past hour the function will clasify it as the past
+            if(blockPastTime < currentTime){
+                $(this).addClass('past');
+            } 
+            
+            //classifies it time as the present if current time is the same as the block time 
+            else if(blockPastTime == currentTime){
+                $(this).removeClass('past');
+                $(this).removeClass('future');
+                $(this).addClass('present');
+            }
+
+            //classifies it time as the future
+            else{
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+
+        })// closes loop for the schedule chart
+
+    }// closes fucntion that checks for past, present or future time
+
+    
+
+    
+
+    
+})// closes dom
